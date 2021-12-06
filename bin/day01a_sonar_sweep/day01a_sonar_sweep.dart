ccb8acafb743e3cdd4c3d244ed2d1d598ed76ede
Hagen Schendel <mail@hschendel.de>
2021-12-06 17:43:50 -0500

    day 01a sonar sweep

---
+++ bin/day01a_sonar_sweep/day01a_sonar_sweep.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'dart:convert';
+
+void main(List<String> arguments) async {
+  final file = File('input.txt');
+  Stream<String> lines =
+      file.openRead().transform(utf8.decoder).transform(LineSplitter());
+  try {
+    int previousDepth = 0;
+    var firstLine = true;
+    int increases = 0;
+    await for (final line in lines) {
+      int depth = int.parse(line);
+      if (firstLine) {
+        firstLine = false;
+      } else {
+        if (depth > previousDepth) {
+          increases++;
+        }
+      }
+      previousDepth = depth;
+    }
+    stdout.writeln(increases);
+  } catch (e) {
+    stderr.addError(e);
+  }
+}
